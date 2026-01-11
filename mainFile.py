0e0bee616f539426ad2ca1b353cde2c8bc7ba47d
pawan <pawan.barnwal2@gmail.com>
2026-01-11 21:50:55 +0530

    basic 1

---
+++ mainFile.py
@@ -0,0 +1,13 @@
+# print("Enter input")
+
+# name = input("Enter Input name")
+
+# print(name + "Correct name")
+
+# print(name[1:3])
+
+nam=[1,2,3,44]
+i=0;
+while i<= len(nam)-1:
+	print(nam[i])
+	i+=1
